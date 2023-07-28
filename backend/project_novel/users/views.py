f110b7960d2cdee50ca69e56e7435383e4812d6c
mireu-san <starmireu@gmail.com>
2023-07-28 17:47:05 +0900

    updated app - users, views, models, serializers v1

--- backend/project_novel/users/views.py
+++ backend/project_novel/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User created successfully",
+        }, status=status.HTTP_201_CREATED)
