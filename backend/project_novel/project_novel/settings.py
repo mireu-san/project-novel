d0ee1c2837926ac7f1cc37fadb5cbdecd2734eff
mireu-san <starmireu@gmail.com>
2023-07-30 16:10:49 +0900

    applied cors policy (django) + encryption

--- backend/project_novel/project_novel/settings.py
+++ backend/project_novel/project_novel/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'corsheaders',
     'chatbot',
     'users',
 ]
@@ -51,7 +52,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "project_novel.urls"
 
