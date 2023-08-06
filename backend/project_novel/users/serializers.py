a512e56e4a2b6e88e7523c04540b5839924139d9
mireu-san <starmireu@gmail.com>
2023-08-06 21:17:22 +0900

    reviewed - users serializers, models

--- backend/project_novel/users/serializers.py
+++ backend/project_novel/users/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
     def create(self, validated_data):
