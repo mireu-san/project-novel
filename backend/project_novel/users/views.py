9fac916ccb8b15c3b67b836d7c6f0cf1f2ff5726
Chesed Kim <starmireu@gmail.com>
2023-08-12 22:58:18 +0900

    updated users, views.py LoginView

--- backend/project_novel/users/views.py
+++ backend/project_novel/users/views.py
@@ -5,6 +5,9 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
 
 # 메모: view 로 할까 싶었지만, 특별히 커스텀화가 요구되는게 아닌 이상 viewsets 으로 설정.
 class UserViewSet(viewsets.ModelViewSet):
@@ -23,3 +26,17 @@ class UserViewSet(viewsets.ModelViewSet):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "message": "User created successfully",
         }, status=status.HTTP_201_CREATED)
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username,
+        })
+    
