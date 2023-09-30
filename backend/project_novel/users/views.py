59480b283bd3f9ff8d49c159b282265f2d11c8b4
Chesed Kim <starmireu@gmail.com>
2023-09-30 17:33:37 +0900

    users - updated permission, UserViewSet, IsSelfOrAdmin

--- backend/project_novel/users/views.py
+++ backend/project_novel/users/views.py
@@ -5,29 +5,34 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, BasePermission
 from rest_framework.authtoken.views import ObtainAuthToken, APIView
 from rest_framework.authtoken.models import Token
 
 
 # 메모: view 로 할까 싶었지만, 특별히 커스텀화가 요구되는게 아닌 이상 viewsets 으로 설정.
+class IsSelfOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Allow user to view/update/delete their own profile
+        if request.user == obj:
+            return True
+        # Allow admins to view/update/delete any profile
+        if request.user.is_staff:
+            return True
+        return False
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]  # This allows unauthenticated users to create an account
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        # set password before saving
-        user.set_password(request.data['password'])
-        user.save()
-        return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "message": "User created successfully",
-        }, status=status.HTTP_201_CREATED)
-    
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action in ['list', 'retrieve', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAuthenticated, IsSelfOrAdmin]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]    
 
 class SignupView(CreateAPIView):
     serializer_class = UserSerializer
