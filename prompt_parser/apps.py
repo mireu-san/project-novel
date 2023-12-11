9f9bfc7ca406a5786431a572e72498a9c0a3ac9e
Chesed Kim <starmireu@gmail.com>
2023-12-11 11:12:06 +0900

    build : renamed celeryapp -> prompt_parser

--- prompt_parser/apps.py
+++ prompt_parser/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CeleryAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'celeryapp'
+class PromptParserConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "prompt_parser"
